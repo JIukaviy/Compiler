1a3cff30d398094cb1ec8f08bb3ae75329d08ec1
JIukaviy-PC <dobryniny07@mail.ru>
2015-11-26 18:58:11 +1000

    Добавить отдельную проверку типа для ST_PTR

--- Compiler/type_conversion.cpp
+++ Compiler/type_conversion.cpp
@@ -5,8 +5,14 @@
 #include <map>
 #include <set>
 
+bool eq_types(type_base_ptr a, type_base_ptr b) {
+	return a == b || a == ST_PTR && b == ST_PTR && 
+		static_pointer_cast<sym_type_ptr_t>(a)->get_element_type() ==
+		static_pointer_cast<sym_type_ptr_t>(b)->get_element_type();
+}
+
 expr_t* add_cast(expr_t* src, type_ptr dst) {
-	if (src->get_type()->get_base_type() == dst->get_base_type())
+	if (eq_types(src->get_type()->get_base_type(),dst->get_base_type()))
 		return src;
 	expr_cast_t* res = new expr_cast_t();
 	res->set_operand(src, dst);
@@ -42,7 +48,7 @@ void align_types(expr_t** left, expr_t** right) {
 	type_ptr left_type = (*left)->get_type();
 	type_ptr right_type = (*right)->get_type();
 
-	if (left_type->get_base_type() == right_type->get_base_type())
+	if (eq_types(left_type->get_base_type(), right_type->get_base_type()))
 		return;
 
 	type_ptr max_type = get_max_type(left_type, right_type);
