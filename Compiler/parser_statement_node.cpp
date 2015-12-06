90118691faae95213bacee9bb46236d5f82540e1
JIukaviy-PC <dobryniny07@mail.ru>
2015-12-06 17:58:58 +1000

    Исключение для void в stmt_expr::asm_generate_code

--- Compiler/parser_statement_node.cpp
+++ Compiler/parser_statement_node.cpp
@@ -64,7 +64,8 @@ void stmt_expr_t::print_l(ostream& os, int level) {
 
 void stmt_expr_t::asm_generate_code(asm_cmd_list_ptr cmd_list, int offset) {
 	expression->asm_get_val(cmd_list);
-	cmd_list->add(AR_ESP, new_var<int>(asm_generator_t::alignment(expression->get_type()->get_size())));
+	if(expression->get_type() != ST_VOID)
+		cmd_list->add(AR_ESP, new_var<int>(asm_generator_t::alignment(expression->get_type()->get_size())));
 }
 
 void stmt_decl_t::print_l(ostream& os, int level) {
