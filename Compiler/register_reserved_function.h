e7bb35fbaf67fa2643a8e96d588d206d177b292f
JIukaviy-PC <dobryniny07@mail.ru>
2015-12-15 00:26:13 +1000

    Добавить зарезервированную функцию scanf

--- Compiler/register_reserved_function.h
+++ Compiler/register_reserved_function.h
@@ -6,6 +6,14 @@ void check_args_printf(vector<expr_t*> &args, expr_t* func, ...) {
 		throw SemanticError("Printf operator requires pointer to char as first parameter", args[0]->get_pos());
 }
 
+void check_args_scanf(vector<expr_t*> &args, expr_t* func, ...) {
+	if (args.size() < 2)
+		throw SemanticError("Scanf operator requires at least two parameters", func->get_pos());
+	for (int i = 0; i < args.size(); i++)
+		if (args[i]->get_type() != ST_PTR)
+			throw SemanticError("Scanf operator can pass only pointer type parameters", args[i]->get_pos());
+}
+
 void check_args(vector<expr_t*> &args, expr_t* func, ...) {
 	vector<type_ptr> func_arg_types;
 	SYM_TYPE* curr_type = (SYM_TYPE*)(&func);
@@ -20,4 +28,5 @@ void check_args(vector<expr_t*> &args, expr_t* func, ...) {
 
 #ifdef reg_res_func
 reg_res_func(PRINTF, printf, crt_printf, check_args_printf, ST_INTEGER, 0)
+reg_res_func(SCANF, scanf, crt_scanf, check_args_scanf, ST_INTEGER, 0)
 #endif
